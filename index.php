f37cfd7b55963dd5f378bef46c1d4a4e679e0035
Csar <gr14079@ues.edu.sv>
2022-07-02 11:30:55 -0600

    php

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-     <h1>cesar Alberto Gonzalez Rivera</h1>
-     <h2> "git push origin master" agregar los cambios al repositorio de github"</h2>
-    
-</body>
-</html>
+<?php
+  echo "se agrego la estructura "
+?>
